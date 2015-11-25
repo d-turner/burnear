91f09fc0cff02413a04713297f44875b821a0992
Daniel Turner <dturner@tcd.ie>
2015-11-25 15:03:34 +0000

    updated user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :username,
             presence: true,
             uniqueness: true
-  validates :password_hash => false,
+  validates :password_hash,
             presence: true
   validates :email,
             presence: true,
