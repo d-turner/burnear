885031c393a706f8c8e7f611c93c83bde067fe7d
Daniel Turner <dturner@tcd.ie>
2015-11-25 15:01:11 +0000

    Updated seed data and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125014926) do
-
-  create_table "chat_user", id: false, force: :cascade do |t|
-    t.integer "userId"
-    t.integer "chatId"
-  end
+ActiveRecord::Schema.define(version: 20151125133037) do
 
   create_table "chats", force: :cascade do |t|
-    t.string   "name"
-    t.text     "owner"
+    t.text     "name"
+    t.text     "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "chats_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "chat_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text     "content"
+    t.integer  "Chat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -34,8 +35,11 @@ ActiveRecord::Schema.define(version: 20151125014926) do
   create_table "users", force: :cascade do |t|
     t.text     "firstName"
     t.text     "lastName"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "username"
+    t.text     "password_hash"
+    t.text     "email"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
