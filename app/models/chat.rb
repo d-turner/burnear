a23862e77d48b5bedba8c349c0a41356b83db726
Daniel Turner <dturner@tcd.ie>
2015-11-25 15:04:22 +0000

    Updated chat validations

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,7 @@ class Chat < ActiveRecord::Base
   has_and_belongs_to_many :users
   validates :name,
             presence: true,
-            length: {maximum: 140}
+            length: {maximum: 10}
+  validates :admin,
+            presence: true
 end
